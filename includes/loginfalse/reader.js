5216ea2b40b0e28a8075e4f276647811a20c8c14
Vũ Nguyễn Thành Tâm <rfechinonguyen@gmail.com>
2020-11-14 23:47:25 +0700

    Delete reader.js

--- includes/loginfalse/reader.js
+++
@@ -1,9 +0,0 @@
-const { createInterface } = require('readline');
-const { once } = require('events');
-
-const reader = async () => {
-	var rl = createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
-}
