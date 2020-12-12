4e519975ced61d57bd26208bdaec81d8b9840e4a
Vũ Nguyễn Thành Tâm <rfechinonguyen@gmail.com>
2020-12-13 02:30:30 +0700

    Update qr.js

--- commands/qr.js
+++ commands/qr.js
@@ -11,21 +11,14 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args }) {
-  var fs = require("fs-extra")
-  var text = args.join(" ")
-      if(!text) return api.sendMessage("Nhập những thứ bạn muốn thêm vào mã qr",event.threadID);
-			var opt = {
-				errorCorrectionLevel: 'H',
-				type: 'image/png',
-				quality:0.3,
-				scale:50,
-				margin:1,
-				color:{dark:"#000000",light:"#ffffff"}
-		}
-		 api.sendTypingIndicator(event.threadID,() =>	require('qrcode').toFile(__dirname + '/cache/qr.png', text , opt, (err) => {
-			if (err) throw err;
-			api.sendMessage({
-				attachment: fs.createReadStream(__dirname + '/cache/qr.png')
-			},event.threadID, () => fs.unlinkSync(__dirname + '/cache/qr.png'), event.messageID);
-		}))}
-    
+	const { createReadStream, unlinkSync } = require("fs-extra")
+	const text = args.join(" ")
+	if(!text) return api.sendMessage("Nhập những thứ bạn muốn thêm vào mã qr",event.threadID);
+	var opt = { errorCorrectionLevel: 'H', type: 'image/png', quality:0.3, scale:50, margin:1, color:{ dark:"#000000",light:"#ffffff" } };
+	 api.sendTypingIndicator(event.threadID, () => require('qrcode').toFile(__dirname + '/cache/qr.png', text, opt, (err) => {
+		if (err) throw err;
+		api.sendMessage({
+			attachment: createReadStream(__dirname + '/cache/qr.png')
+		},event.threadID, () => unlinkSync(__dirname + '/cache/qr.png'), event.messageID);
+	}))
+}
