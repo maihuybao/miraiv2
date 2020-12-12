3e48e76b479f8f32197d1f90f33a11a7810d875d
Vũ Nguyễn Thành Tâm <rfechinonguyen@gmail.com>
2020-12-13 02:31:47 +0700

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -6,13 +6,12 @@ module.exports.config = {
 	description: "Đổi biệt danh trong nhóm của bạn hoặc của người bạn tag",
 	commandCategory: "general",
 	usages: "setname [name]",
-	cooldowns: 3,
-	dependencies: []
+	cooldowns: 3
 };
 
 module.exports.run = async function({ api, event, args }) {
-  var name = args.join(" ")
-var mention = Object.keys(event.mentions)[0];
-         if (!mention) return api.changeNickname(`${name}`, event.threadID, event.senderID);
-         if (name.indexOf("@") !== -1) return api.changeNickname(`${name.replace(event.mentions[mention], "")}`, event.threadID, mention);
-		  }
+	const name = args.join(" ")
+	const mention = Object.keys(event.mentions)[0];
+	if (!mention) return api.changeNickname(`${name}`, event.threadID, event.senderID);
+	if (name.indexOf("@") !== -1) return api.changeNickname(`${name.replace(event.mentions[mention], "")}`, event.threadID, mention);
+}
