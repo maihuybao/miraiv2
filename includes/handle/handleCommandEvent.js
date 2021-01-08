094f9bdecae57f4798bbd75b46a5c705dbb40251
Catalizcs <rfechinonguyen@gmail.com>
2021-01-08 20:32:38 +0700

    move load file, fix something idk

--- includes/handle/handleCommandEvent.js
+++ includes/handle/handleCommandEvent.js
@@ -3,8 +3,8 @@ const logger = require("../../utils/log.js");
 module.exports = function({ api, __GLOBAL, client, models, User, Thread, Currency, utils }) {
 	return async function({ event }) {
 		if (client.userBanned.has(event.senderID) || client.threadBanned.has(event.threadID)) return;
-		let commands = client.commands.values();
-		for (let command of commands) {
+		var commands = client.commands.values();
+		for (var command of commands) {
 			if (command.event) {
 				try {
 					command.event({ api, event, client, __GLOBAL, models, utils });
