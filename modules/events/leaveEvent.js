e361cb1bc4efeff2ac09d994d26fa1d972d30175
CatalizCS <rfechinonguyen@gmail.com>
2021-02-28 21:42:23 +0700

    fix leaveEvent

--- modules/events/leaveEvent.js
+++ modules/events/leaveEvent.js
@@ -10,13 +10,7 @@ module.exports.run = async function({ api, event, Users, Threads, client }) {
 	let name, msg, formPush
 	const { createReadStream, existsSync, mkdirSync } = require("fs-extra");;
 	let settings = client.threadSetting.get(event.threadID) || {};//(await Threads.getData(event.threadID)).settings;
-	try {
-		name = (await Users.getData(event.logMessageData.leftParticipantFbId)).name;
-		if (typeof name == "undefined") throw Error();	
-	}
-	catch {
-		name = (await api.getUserInfo(event.logMessageData.leftParticipantFbId))[event.logMessageData.leftParticipantFbId].name;
-	}
+	let name = (await Users.getData(event.logMessageData.leftParticipantFbId)).name || (await api.getUserInfo(event.logMessageData.leftParticipantFbId))[event.logMessageData.leftParticipantFbId].name
 	let type = (event.author == event.logMessageData.leftParticipantFbId) ? "tự rời" : "bị quản trị viên đá";
 	(typeof settings.customLeave == "undefined") ? msg = "{name} Đã {type} khỏi nhóm" : msg = settings.customLeave;
 	msg = msg
